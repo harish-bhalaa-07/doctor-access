1696a16ab7aadbd8c13c681cbd955a718c005d80
Harish Bhalaa <107175621+harish-bhalaa-07@users.noreply.github.com>
2024-03-02 09:32:25 +0530

    Add files via upload

---
+++ server/database/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const mongo = "mongodb+srv://harishbhalaa:harish@backend.w8koxqb.mongodb.net/"
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect(mongo, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
